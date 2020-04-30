467ee96154ac03a1503393d329556dfaa5d19dfa
xavierad <xdias_28@hotmail.com>
2020-04-30 21:12:41 +0100

    Uploads

--- src/bnc/MDL.java
+++ src/bnc/MDL.java
@@ -5,6 +5,10 @@ package bnc;
  */
 public class MDL extends LL {
 
+    public MDL(Data train_data, Data test_data) {
+        super(train_data, test_data);
+    }
+
     @Override
     public void score() {        
         super.score();
--- src/load_data/ReadCSV.java
+++ src/load_data/ReadCSV.java
@@ -4,7 +4,6 @@ package load_data;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.LinkedList;
 
 /**
