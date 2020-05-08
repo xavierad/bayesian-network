973c8ae7b24ea38fa199ef9db993ef60fc3a2e94
mefool <vicentegarciafernandes@tecnico.ulisboa.pt>
2020-05-08 17:21:56 +0000

    pls

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,26 +5,26 @@ import load_data.*;
 
 /* ****************************************************************************************
  * (Xavier)
- * Para compilar: >/src javac bnc/*.java load_data/*.java main/*.java 
+ * Para compilar: >/src javac bnc/*.java load_data/*.java main/*.java
  * Para correr: >/src java main.Main bias-train.csv bias-test.csv <score>
  * Para gerar o ficheiro .jarpara gerar fichero jar
  * - jar cfm lab3.jar manif.txt lab3/* main/*
  * - java -jar hello.jar   --> para compilar o .jar
- * - jar tf hello.jar      --> para ver o conteúdo 
- * 
+ * - jar tf hello.jar      --> para ver o conteúdo
+ *
  * Para compilar: (Vicente)
  *   /. javac @files.txt  (files.txt contêm os path para os varios .java este é atualizado via
  *                         dir /b /s | findstr \.java > files.txt )
  * Para correr:
  *  /src java main/Main ../bin/bias-train.csv ../bin/bias-test.csv LL
- * 
- * 
+ *
+ *
  * DÚVIDAS:
  * []
  * []
- * 
- * 
- * 
+ *
+ *
+ *
  * A FAZER:
  * []
  * []
@@ -40,12 +40,12 @@ import load_data.*;
 
 public class Main {
     public static void main(String[] args) {
-	
+
         if(args.length != 3) {
             System.out.println("Expected 3 arguments instead of " + args.length + ".");
             System.exit(1);
         }
-        
+
         /* Score strategy */
         if(args[2].equals("LL")) {
             //strategy LL
@@ -58,49 +58,15 @@ public class Main {
         else {
             System.out.println("No score chosen!");
             System.exit(1);
-        }     
-        
+        }
+
         ReadCSV read_train = new ReadCSV(args[0]);
         Dataset train_data = new Dataset(read_train.ReadFile());
-        ReadCSV read_test = new ReadCSV(args[1]);        
+        ReadCSV read_test = new ReadCSV(args[1]);
         Dataset test_data = new Dataset(read_test.ReadFile());
 
        /* System.out.println("Train Data: \n" + train_data);
         System.out.println("Test Data: \n" + test_data);*/
-
-	/** Structure Learning **/
-	/* Process data - into Nijkc and maybe seperate Xi */
-
-	/* Get Weigths - acoording to the model chosen */
-	
-	/* Build Weighted Graph */
-
-	/* Transform to MaxSpanningTree (Undirected) */
-
-	/* Transform to Directed Tree - arbitearly chosen root */
-
-	/* (?) Add leafs to tree with C values */
-
-	/** Parameter Learning (or just BNC) **/
-	/* Get OFE */
-
-	/* Get ClassOFE */
-
-	/* Predict (?)*/
-
-	/** OU
-	    Classifer example = new BayesNetworkClassifierUsingLL(train_data)
-	    (...)
-	    results = example.predict(test_data);
-	 ** OU ainda
-	    Classifier example = new BayesNetworkClassifier(LLcost);
-	    Classifier example = new BayesNetworkClassifier(MDLcost);
-	    
-	    interface ICostFunction {
-	       
-	    }
-    */
-    
 
         /* Possível estrutura para o main - mutável
 
@@ -115,15 +81,15 @@ public class Main {
         else {
             System.out.println("No score chosen!");
             System.exit(1);
-        }  
+        }
 
         ReadCSV read_train = new ReadCSV(args[0]);
         Dataset train_data = new Dataset(read_train.ReadFile());
-        ReadCSV read_test = new ReadCSV(args[1]);        
+        ReadCSV read_test = new ReadCSV(args[1]);
         Dataset test_data = new Dataset(read_test.ReadFile());
 
         IClassifier bnc = new BNC(cf);
-        long start = System.currentTimeMillis();        
+        long start = System.currentTimeMillis();
         bnc.build(train_data);
         long finish = System.currentTimeMillis();
         long timeElapsed = finish - start;
