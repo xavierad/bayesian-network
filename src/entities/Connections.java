ac45efaf2f77fca547e40b284e946bc8bda116d0
afonsoalmeida4 <48207019+afonsoalmeida4@users.noreply.github.com>
2020-05-12 14:43:32 +0100

    Update Connections.java

--- src/entities/Connections.java
+++ src/entities/Connections.java
@@ -1,5 +1,9 @@
 package entities;
 
+/**
+ * This constructor helps in how we can save the nodes connections in a list.
+ * That contains the constructor´s getters and setters to access your fields.
+ */
 public class Connections {
     private Integer father;
     private Integer son;
