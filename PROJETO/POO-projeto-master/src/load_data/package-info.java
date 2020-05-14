cace328a91f8caf7cf871b002f98830b10198b48
mefool <vicentegarciafernandes@tecnico.ulisboa.pt>
2020-05-14 14:59:19 +0000

    Clean up.

--- PROJETO/POO-projeto-master/src/load_data/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package provides a class nessacry to read a CSV file.
- */
-package load_data;
--- PROJETO/POO-projeto-master/src/metrics/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package provides an interface and a class necessary to evaluate a classifier making the use of some implemented metrics.
- */
-package metrics;
